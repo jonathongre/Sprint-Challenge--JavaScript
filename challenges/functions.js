ad9a7394e9f1b1e9606b273d3b08ae357b4a203e
Jonathon Green <ericg9811@gmail.com>
2019-06-14 11:40:59 -0500

    answered closures question in functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -37,7 +37,7 @@ consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Both the function `nestedfunction()` and the variable `internal` are nested in the same scope.
 
 
 const external = "I'm outside the function";
